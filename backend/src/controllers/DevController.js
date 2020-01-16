d776a58541edf64bb7148dfbd65cca9ad218fac8
Lucas Fujiwara <lucastfujiwara@gmail.com>
2020-01-16 19:51:16 -0300

    Devs + Search CRUD

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,71 @@
+const axios = require('axios')
+const Dev = require('../models/Dev')
+const parseArrayAsString = require('../utils/parseStringAsArray')
+
+module.exports = {
+  index: async (request, response) => {
+    const devs = await Dev.find()
+    return response.json(devs)
+  },
+  store: async (request, response) => {
+    const { github_username, techs, latitude, longitude } = request.body
+
+    let dev = await Dev.findOne({ github_username })
+
+    if (!dev) {
+      const apiResponse = await axios.get(
+        `https://api.github.com/users/${github_username}`
+      )
+
+      const {
+        name = apiResponse.data.login,
+        avatar_url,
+        bio,
+      } = apiResponse.data
+      const techsArray = parseArrayAsString(techs)
+      const location = { type: 'Point', coordinates: [longitude, latitude] }
+
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        location,
+        bio,
+        techs: techsArray,
+      })
+    }
+    return response.json(dev)
+  },
+  update: async (request, response) => {
+    const { github_username } = request.body
+    const dev = await Dev.findOne({ github_username })
+
+    if (!dev) {
+      response.code(404)
+      return response.json({ message: 'User not found' })
+    }
+
+    Object.entries(request.body)
+      .filter(entry => entry[0] !== 'github_username')
+      .forEach(([k, v]) => (dev[k] = v))
+
+    dev.save()
+
+    return response.json(dev)
+  },
+  destroy: async (request, response) => {
+    const { github_username } = request.query
+    const dev = await Dev.findOne({ github_username })
+
+    if (!dev) {
+      response.code(404)
+      return response.json({ message: 'User not found' })
+    }
+
+    dev
+      .remove()
+      .then((err, old_dev) =>
+        response.json(err ? err : { message: 'Deleted', old_dev })
+      )
+  },
+}
