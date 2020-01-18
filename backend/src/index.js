1a259c064101e3df10b0eee95c2e8b746faeb9e5
Lucas Fujiwara <lucastfujiwara@gmail.com>
2020-01-17 23:06:59 -0300

    Aula front-end

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express()
 
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGODB_CONNECTION_URL, {
 })
 
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 
 app.listen(1337, '0.0.0.0')
---
+++ web/src/components/DevForm/index.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+
+function DevForm({ onSubmit }) {
+  const [github_username, setGithubUsername] = useState("");
+  const [techs, setTechs] = useState("");
+
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      err => console.log(err),
+      { timeout: 3000 }
+    );
+  }, []);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    await setGithubUsername("");
+    await setTechs("");
+    await onSubmit({
+      github_username,
+      techs,
+      latitude,
+      longitude
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="input-block">
+        <label htmlFor="github_username">Usuário do GitHub</label>
+        <input
+          name="github_username"
+          id="github_username"
+          value={github_username}
+          onChange={e => setGithubUsername(e.target.value)}
+          required
+        ></input>
+      </div>
+
+      <div className="input-block">
+        <label htmlFor="techs">Tecnologias</label>
+        <input
+          name="techs"
+          id="techs"
+          value={techs}
+          onChange={e => setTechs(e.target.value)}
+          required
+        ></input>
+      </div>
+
+      <div className="input-group">
+        <div className="input-block">
+          <label htmlFor="latitude">Latitude</label>
+          <input
+            type="number"
+            name="latitude"
+            id="latitude"
+            value={latitude}
+            onChange={e => setLatitude(e.target.value)}
+            step={0.0001}
+            required
+          />
+        </div>
+        <div className="input-block">
+          <label htmlFor="longitude">Longitude</label>
+          <input
+            type="number"
+            name="longitude"
+            id="longitude"
+            value={longitude}
+            onChange={e => setLongitude(e.target.value)}
+            step={0.0001}
+            required
+          />
+        </div>
+      </div>
+      <button type="submit">Salvar</button>
+    </form>
+  );
+}
+
+export default DevForm;
